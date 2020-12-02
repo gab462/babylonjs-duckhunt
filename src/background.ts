ba61ab80453188e93d3510102cde5cbd8d26224f
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-12-02 18:39:09 -0300

    Added shot animation

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,45 @@
-import { Mesh, MeshBuilder, Scene, StandardMaterial, Texture } from "babylonjs";
+import { Mesh, MeshBuilder, Scene, StandardMaterial, Texture, Vector3 } from "babylonjs";
 
 export class Background {
-	mesh: Mesh;
+	private mesh: Mesh;
+	private shotFrameNumber: number;
+	private shotFrames: number;
+	private shotMark: Mesh;
 
 	constructor(texture : string, scene : Scene) {
 		this.mesh = MeshBuilder.CreatePlane("bg", {size: 10}, scene);
+
 		this.mesh.material = new StandardMaterial("background", scene);
 		this.mesh.material.backFaceCulling = false;
 		(<StandardMaterial>this.mesh.material).diffuseTexture = new Texture(texture, scene);
 		(<StandardMaterial>this.mesh.material).diffuseTexture.hasAlpha = true;
 		(<StandardMaterial>this.mesh.material).disableLighting = false;
+
+		this.shotFrameNumber = 0;
+		this.shotFrames = 5;
+		this.shotMark = MeshBuilder.CreatePlane("shot", {size: 0.5}, scene);
+		this.shotMark.setEnabled(false);
+	}
+
+	getMesh() : Mesh {
+		return this.mesh;
+	}
+
+	shoot(position : Vector3) : void {
+		this.shotFrameNumber = this.shotFrames;
+		this.shotMark.setEnabled(true);
+		this.shotMark.position = position;
+		this.shotMark.rotation.y =  Math.PI;
+		(<StandardMaterial>this.mesh.material).disableLighting = true;
+	}
+
+	animate() : void {
+		if (this.shotFrameNumber > 0) {
+			this.shotFrameNumber--;
+		}
+		else {
+			this.shotMark.setEnabled(false);
+			(<StandardMaterial>this.mesh.material).disableLighting = false;
+		}
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,21 @@ const view = document.getElementById("view") as HTMLCanvasElement;
 const engine = new Engine(view, true, { preserveDrawingBuffer: true, stencil: true });
 const scene = new Scene(engine);
 const light = new HemisphericLight("light", new Vector3(0, 10, 0), scene);
-const camera = new FreeCamera("camera", new Vector3(0,0,15), scene);
+const camera = new FreeCamera("camera", new Vector3(0, 0, 15), scene);
 camera.attachControl(view, true);
 
 const bg = new Background("textures/bg.png", scene);
-camera.lockedTarget = bg.mesh;
+camera.lockedTarget = bg.getMesh();
+
+scene.onPointerDown = (_, pickResult) => {
+	if (pickResult.hit) {
+		bg.shoot(new Vector3 ( pickResult.pickedPoint.x, pickResult.pickedPoint.y, 1 ) );
+	}
+};
 
 engine.runRenderLoop(() => {
-    scene.render();
+	bg.animate();
+	scene.render();
 });
 
 window.addEventListener("resize", () => {
