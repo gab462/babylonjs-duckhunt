98013cdcfc97179de594012fca029cf1c946eb1f
Luiz Buffolo <Luiz.Buffolo@fit-tecnologia.org.br>
2020-12-04 20:40:20 -0300

    Duck animation completed

--- src/duck.ts
+++ src/duck.ts
@@ -6,19 +6,24 @@ export class Duck {
 	private startPoint: Vector3;
 	private upperLeft: Vector3;
 	private bottomRight: Vector3;
+	private randomX: number;
 
-	constructor(texture : string, scene : Scene) {
-		this.mesh = MeshBuilder.CreatePlane("duck", {size: 1}, scene);
+	constructor(upperLeft: Vector3, bottomRight: Vector3, scene: Scene) {
+		this.mesh = MeshBuilder.CreatePlane("duck", { size: 1 }, scene);
 
 		this.mesh.material = new StandardMaterial("duckSprite", scene);
 		this.mesh.material.backFaceCulling = false;
-		(<StandardMaterial>this.mesh.material).diffuseTexture = new Texture(texture, scene);
+		(<StandardMaterial>this.mesh.material).diffuseTexture = new Texture("textures/duck.png", scene);
 		(<StandardMaterial>this.mesh.material).diffuseTexture.hasAlpha = true;
+		this.startPoint = new Vector3(0, -3, 0.1);
+		this.mesh.setEnabled(false);
+		this.upperLeft = upperLeft;
+		this.bottomRight = bottomRight;
 	}
 
 	private isInBounds(): boolean {
-		if(this.mesh.position.x > this.upperLeft.x && this.mesh.position.x < this.bottomRight.x) {
-			if(this.mesh.position.y < this.upperLeft.y && this.mesh.position.y > this.bottomRight.y) {
+		if (this.mesh.position.x > this.upperLeft.x && this.mesh.position.x < this.bottomRight.x) {
+			if (this.mesh.position.y < this.upperLeft.y && this.mesh.position.y > this.bottomRight.y) {
 				return true;
 			}
 		}
@@ -27,17 +32,26 @@ export class Duck {
 
 	public start(): void {
 		this.isActive = true;
-		this.mesh.position = this.startPoint;
+		this.mesh.position.x = this.startPoint.x;
+		this.mesh.position.y = this.startPoint.y;
 		this.mesh.setEnabled(true);
+		this.randomX = (Math.random() * 0.2) - 0.1;
+		if(this.randomX < 0){
+			this.mesh.rotation.y = Math.PI;
+		}
+		else{
+			this.mesh.rotation.y = 0;
+		}
 	}
 
 	public animate(): void {
-		if(!this.isInBounds() || !this.mesh.isEnabled()) { // Also checks if it was disabled externally
+		if (!this.isInBounds() || !this.mesh.isEnabled()) { // Also checks if it was disabled externally
 			this.isActive = false;
 			this.mesh.setEnabled(false);
 		}
-		else if(this.isActive) {
-			// Animate position
+		else if (this.isActive) {
+			this.mesh.position.y += 0.1;
+			this.mesh.position.x += this.randomX;
 		}
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { FreeCamera, Engine, HemisphericLight, Scene, Vector3 } from "babylonjs";
 import { Background } from "./background";
 import { Dog } from "./dog";
+import { Duck } from "./duck";
 
 const view = document.getElementById("view") as HTMLCanvasElement;
 const engine = new Engine(view, true, { preserveDrawingBuffer: true, stencil: true });
@@ -10,12 +11,16 @@ const camera = new FreeCamera("camera", new Vector3(0, 0, 15), scene);
 camera.attachControl(view, true);
 
 const bg = new Background("textures/bg.png", scene);
+const dog = new Dog(scene);
+const duck1 = new Duck(new Vector3(-5, 5, 0), new Vector3(5, -5, 0), scene);
+const duck2 = new Duck(new Vector3(-5, 5, 0), new Vector3(5, -5, 0), scene);
+const duck3 = new Duck(new Vector3(-5, 5, 0), new Vector3(5, -5, 0), scene);
 
 camera.lockedTarget = bg.getMesh();
 
 scene.onPointerDown = (_, pickResult) => {
 	if (pickResult.hit) {
-		bg.shoot(new Vector3 ( pickResult.pickedPoint.x, pickResult.pickedPoint.y, 0.1 ) );
+		bg.shoot(new Vector3(pickResult.pickedPoint.x, pickResult.pickedPoint.y, 0.1));
 	}
 };
 
